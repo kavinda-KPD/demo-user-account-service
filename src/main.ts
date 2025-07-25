b6e3f6c85e40aca07638ae683b80cb2001038930
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-07-25 10:23:29 +0530

    initiate the constants

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { USER_ACCOUNT_QUEUE } from './constants';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.RMQ,
       options: {
-        // urls: ['amqp://guest:guest@localhost:5672'],
-        urls: [
-          // `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASS}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`,
-          `${process.env.RABBITMQ_URL}`,
-        ],
+        urls: [`${process.env.RABBITMQ_URL}`],
         queue: USER_ACCOUNT_QUEUE,
         queueOptions: {
           durable: true,
